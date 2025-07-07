a14d2d23d7d60aa1bf138bcfa2e8ac86b66269a1
doom99987 <126832091+doom99987@users.noreply.github.com>
2025-07-07 16:23:25 -0700

    paddleMovmentScript

---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    [SerializeField] private KeyCode K1Left = KeyCode.A;
+    [SerializeField] private KeyCode K1Right = KeyCode.D;
+    [SerializeField] private KeyCode k2Left = KeyCode.LeftArrow;
+    [SerializeField] private KeyCode k2Right = KeyCode.RightArrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(0f, -4.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(K1Left) || Input.GetKey(k2Left) && transform.position.x > -5.5f)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-10f, 0f);
+        }
+        else if (Input.GetKey(K1Right) || Input.GetKey(k2Right) && transform.position.x < 5.5f)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(10f, 0f);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+        }
+    }
+}
