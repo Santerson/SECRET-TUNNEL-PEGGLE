b4a0f9be602795bb450934c541497b150053d020
doom99987 <126832091+doom99987@users.noreply.github.com>
2025-07-08 16:19:28 -0700

    MoreLevelController

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    private readonly int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
