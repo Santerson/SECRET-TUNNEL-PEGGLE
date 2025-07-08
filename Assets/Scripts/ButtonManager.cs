9c8553d9ab23e54315f2838af70cb1aa4f95c762
doom99987 <126832091+doom99987@users.noreply.github.com>
2025-07-08 14:59:04 -0700

    MoreScenes

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -15,4 +16,28 @@ public class ButtonManager : MonoBehaviour
     {
         
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is quitting");
+    }
+    public void GoToStartScreen()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+    
+    public void GoToLvl1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void GoToTips()
+    {
+               SceneManager.LoadScene("Tips");
+    }
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
 }
