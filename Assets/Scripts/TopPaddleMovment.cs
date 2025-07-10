d6f813720bfc7734773a5b572753d4d1aa9d1b20
doom99987 <126832091+doom99987@users.noreply.github.com>
2025-07-10 14:44:30 -0700

    e

--- Assets/Scripts/TopPaddleMovment.cs
+++ Assets/Scripts/TopPaddleMovment.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class TopPaddleMovment : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1f;
+    private float speed = 4f;
 
     [SerializeField]
-    private float amplitude = 3f;
+    private float amplitude = 100f;
 
 
     // The original starting position of the object
@@ -21,7 +21,7 @@ public class TopPaddleMovment : MonoBehaviour
     private void Update()
     {
        
-        float xOffset = Mathf.Sin(Time.time * speed) * amplitude;
+        float xOffset = Mathf.Sin(Time.deltaTime * speed) * amplitude + 3;
 
      
         transform.position = startPosition + Vector3.right * xOffset;
