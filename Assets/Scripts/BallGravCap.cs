12f5f3bb14f7195596aa74e966d4df69cc93e370
Santerson <santacap2019@gmail.com>
2025-07-11 15:53:27 -0700

    asdf

--- Assets/Scripts/BallGravCap.cs
+++ Assets/Scripts/BallGravCap.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BallGravCap : MonoBehaviour
 {
     [SerializeField] private float maxYSpeed = 10f;
+    float currSpeed = 10f;
+    [SerializeField] float stallTime = 1f;
     Rigidbody2D rb = null;
     private void Start()
     {
@@ -12,23 +14,38 @@ public class BallGravCap : MonoBehaviour
         if (rb == null)
         {
             Debug.LogError("Rigidbody2D component is not attached to the GameObject. Please add it.");
-        }   
+        }
+        StartCoroutine(startLate());
+    }
+
+    IEnumerator startLate()
+    {
+        currSpeed = 0;
+        
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(stallTime);
+        currSpeed = maxYSpeed;
     }
 
     void Update()
     {
+        if (currSpeed == 0)
+        {
+            transform.position = Vector2.zero;
+            return;
+        }
         if (rb == null)
         {
             return;
         }
         // Cap the vertical speed of the ball
-        if (rb.velocity.y > maxYSpeed)
+        if (rb.velocity.y > currSpeed)
         {
-            rb.velocity = new Vector2(rb.velocity.x, maxYSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, currSpeed);
         }
-        else if (rb.velocity.y < -maxYSpeed)
+        else if (rb.velocity.y < -currSpeed)
         {
-            rb.velocity = new Vector2(rb.velocity.x, -maxYSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, -currSpeed);
         }
     }
 }
